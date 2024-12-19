7673853dc1b16651a9b7dd12bafd4e6110dc7ea3
Liz Wade <11866103+lizwade@users.noreply.github.com>
2024-12-19 15:29:57 +0000

    fix #6

--- models/hoards.js
+++ models/hoards.js
@@ -4,3 +4,24 @@ import { data } from "../libs/coins.js";
 export async function getHoards() {
   return data;
 }
+
+// I don't feel great about this, as I don't understand how it works
+// It was copied from the getAstronauts by name function
+// But includes functionality for partial match, which I'm not sure we need here?
+// Might be a simpler way to write it
+export async function getHoardsByCounty(county) {
+  const lowercased = county.toLowerCase();
+  return data.filter(({ county }) => {
+    return county.toLowerCase().includes(lowercased);
+  });
+}
+
+// export async function getAstronautsByName(search) {
+//     const lowercased = search.toLowerCase();
+//     return astronauts.filter(({ firstName, lastName }) => {
+//       return (
+//         firstName.toLowerCase().includes(lowercased) ||
+//         lastName.toLowerCase().includes(lowercased)
+//       );
+//     });
+//   }
--- routes/hoards.js
+++ routes/hoards.js
@@ -1,12 +1,53 @@
 //routes are defined here
 import express from "express";
 
-import { getHoards } from "../models/hoards.js";
+import { getHoards, getHoardsByCounty } from "../models/hoards.js";
 
 const router = express.Router();
 export default router;
-
+/* OLD WORKING GET for ALL
 router.get("/", async (req, res) => {
   const hoards = await getHoards();
   res.json(hoards);
 });
+*/
+
+router.get("/", async (req, res) => {
+  try {
+    const { county } = req.query;
+    console.log(county);
+    if (!county) {
+      // no county query was sent, so return all the records:
+      const hoards = await getHoards();
+      res.json({
+        success: true,
+        payload: hoards,
+      });
+    }
+    // a county query was sent, so return records for that county only:
+    const hoards = await getHoardsByCounty(county);
+    res.json({
+      success: true,
+      payload: hoards,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+//   try {
+//     const { name } = req.query;
+//     console.log(name);
+//     if (!name) {
+//       const astronauts = await getAstronauts();
+//       return res.json({ success: true, payload: astronauts });
+//     }
+//     const astronauts = await getAstronautsByName(name);
+//     res.json({ success: true, payload: astronauts });
+//   } catch (error) {
+//     res.status(500).json({ success: false, message: error.message });
+//   }
+// });

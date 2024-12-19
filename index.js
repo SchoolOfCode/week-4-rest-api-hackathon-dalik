af38089f64b53e259076ba8d50ad01de9af7b2dd
Kim Lida <88290393+Kinukeo@users.noreply.github.com>
2024-12-19 12:24:41 +0000

    index set up server now listening

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 import express from "express";
 
+import hoardsRouter from "./routes/hoards.js";
+
 const app = express();
+
+app.use(express.json());
+
+app.use("/hoards", hoardsRouter);
+
+export default app;
+
 const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
--- routes/hoards.js
+++ routes/hoards.js
@@ -1,1 +1,9 @@
 //routes are defined here
+import express from "express";
+
+const router = express.Router();
+export default router;
+
+router.get("/", async (req, res) => {
+  res.send("Here are the hoards");
+});
